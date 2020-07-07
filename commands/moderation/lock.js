4431ad6f4a0847b14cb44fb04cf77ab25cb6508a
Jay <34258275+sell@users.noreply.github.com>
2020-07-06 23:09:16 -0400

    added lock command

---
+++ commands/moderation/lock.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "lock",
+    category: "moderation",
+    run: async (client, message, args) => {
+        const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+        if (args[0] === 'on') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: false
+                }).then(() => {
+                    channel.setName(channel.name += `🔒`)
+                })
+            })
+            return message.channel.send('locked all channels');
+        } else if (args[0] === 'off') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: true
+                }).then(() => {
+                        channel.setName(channel.name.replace('🔒', ''))
+                    }
+                )
+            })
+            return message.channel.send('unlocked all channels')
+        }
+    }
+}

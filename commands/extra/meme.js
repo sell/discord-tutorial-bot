2f6cf215019b973369aee0d9d019d6f586c22723
Jay <34258275+sell@users.noreply.github.com>
2020-07-06 23:38:56 -0400

    meme command

--- commands/extra/meme.js
+++ commands/extra/meme.js
@@ -1,23 +1,24 @@
 const axios = require('axios');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     name: "meme",
+    category: "extra",
     run: async (client, message, args) => {
-        const url = "https://some-random-api.ml/meme";
+        const url = 'https://some-random-api.ml/meme';
 
         let data, response;
         try {
             response = await axios.get(url);
             data = response.data;
         } catch (e) {
-            return message.channel.send(`An error occured, please try again!`)
+            return message.channel.send(`An error has occured, try again!`)
         }
 
         const embed = new MessageEmbed()
-            .setTitle(`Random Meme`)
+            .setTitle(`Random Meme: `)
             .setDescription(data.caption)
-            .setColor(`#f3f3f3`)
+            .setColor('#f3f3f3')
             .setImage(data.image)
 
         await message.channel.send(embed)

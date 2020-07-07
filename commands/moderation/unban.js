b44780c8dbc868e4a58bfce4104d55021701f216
Jay <34258275+sell@users.noreply.github.com>
2020-07-06 23:00:45 -0400

    added unban feature

---
+++ commands/moderation/unban.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "unban",
+    category: "moderation",
+    run: async (client, message, args) => {
+
+        const member = args[0];
+
+        if (!member) {
+             return message.channel.send(`Please enter a id!`)
+        }
+
+        try {
+            message.guild.fetchBans().then(bans => {
+                message.guild.members.unban(member)
+            })
+            await message.channel.send(`${member} has been unbanned!`)
+        } catch (e) {
+            return message.channel.send(`An error occured!`)
+        }
+
+    }
+}

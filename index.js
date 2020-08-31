37b6014ec05fb85ffeddc3a505cee919e54d0235
icourt1 <34220392+icourt1@users.noreply.github.com>
2020-08-31 10:05:02 -0500

    map is cooler

--- index.js
+++ index.js
@@ -5,22 +5,10 @@ const client = new Discord.Client({
   });
 const config = require("./botconfig.json");
 const token = config.token;
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
 client.categories = fs.readdirSync("./commands/");
 client.prefix = config.prefix;
 
+['commands', 'events'].map(handler => require(`./handlers/${handler}`)(client));
+['commands', 'aliases'].map(x => client[x] = new Collection());
 
-["command", "event"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-  });
- 
-  client.on('guildMemberAdd', async member => {
-    require("./events/guild/memberAdd")(member)
-  })
-  
-  client.on('guildMemberRemove', async (message) => {
-    require("./events/guild/memberRemove")(message)
-  })
-
-  client.login(token);
+client.login(token);

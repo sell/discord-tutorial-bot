862dbeedf284335516d06c97f8f061f3f7c740d1
Jay <34258275+sell@users.noreply.github.com>
2020-10-28 16:50:30 -0400

    Update unban.js

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -5,20 +5,40 @@ module.exports = {
     category: "moderation",
     run: async (client, message, args) => {
 
-        const member = args[0];
+        const member = await client.users.fetch(args[0]);
 
-        if (!member) {
-             return message.channel.send(`Please enter a id!`)
-        }
+        if (!member) 
+            return message.channel.send('Please enter a users id')
+        
+        
 
-        try {
-            message.guild.fetchBans().then(bans => {
-                message.guild.members.unban(member)
-            })
-            await message.channel.send(`${member} has been unbanned!`)
-        } catch (e) {
-            return message.channel.send(`An error occured!`)
-        }
+        const reason = args[0] ? args.slice(1).join(' ') : 'no reason'
+
+        const embed = new MessageEmbed()
+            .setFooter(`${message.author.tag} | ${message.author.id}`, message.author.displayAvatarURL({ dynamic: true }));
+
+
+        message.guild.fetchBans().then( bans => {
+            const user = bans.find(ban => ban.user.id === member.id);
+            
+            if (user) {
+                embed.setTitle(`Successfully Unbanned ${user.user.tag}`)
+                    .setColor('#00ff00')
+                    .addField('User ID', user.user.id, true)
+                    .addField('User Tag', user.user.tag, true)
+                    .addField('Banned Reason', user.reason != null ? user.reason : 'no reason')
+                    .addField('Unbanned Reason', reason)
+                return message.guild.members.unban( user.user.id, reason ).then(() => message.channel.send(embed));
+            } else {
+                embed.setTitle(`User ${member.tag} isn't banned`)
+                    .setColor('#ff0000')
+                message.channel.send(embed)
+            }
+        }).catch(e => {
+            console.log(e)
+            message.channel.send('An error occurred!')
+        });
+
 
     }
 }

6217541776f1892c235476eb8b46fb2e85295106
Jay <34258275+sell@users.noreply.github.com>
2021-08-07 13:08:01 -0400

    minor fixes

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -10,10 +10,13 @@ module.exports = {
 		if (!args[0]) {
 			return message.channel.send('Please mention a user!');
 		}
+
 		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
+		const reason = args[1] ? args.splice(1).join(' ') : 'No Reason Given';
+
 		try {
-			await member.ban();
+			await member.ban({ reason });
 			return message.channel.send(`${member} has been banned!`);
 		} catch (e) {
 			return message.channel.send('User is not in the server!');
